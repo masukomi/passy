edb52b7ad4803edc1fad2cdd598556ee3fbe5e1d
Rhodes <krhodes@usma1mc-2e6x92.(none)>
2009-03-04 23:00:29 -0500

    Initial commit of firefox extension.
    * still needs gui work but appears to function correctly
    * see TODO.mkdn for how you can help

--- build.sh
+++ build.sh
@@ -4,3 +4,6 @@ echo "Merging the PassyCore with the ubiquity commands."
 echo "Creating ubiquity/passy.js"
 cat ubiquity/passy_ubiquity_section.js core/passy_core.js > ubiquity/passy.js
 cp -v core/passy_core.js web/javascript/
+cp -v core/passy_core.js ff_extension/content/
+cd ff_extension
+./build.sh
